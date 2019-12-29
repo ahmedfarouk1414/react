350bcbe455fb69d13df1dc1cb06fe7c6a78504df
ahmedfarouk1414 <ahmedfarouk141414@gmail.com>
2019-12-29 22:57:10 +0200

    branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hazem emam 14.
+          Edit <code>src/App.js</code> and save to reload ahmed farouk ahmed.
         </p>
         <a
           className="App-link"
